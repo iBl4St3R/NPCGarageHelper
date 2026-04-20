5ba104b5ba0843a24386afbdaafd333d35fe19a3
iBl4St3R <usmati11@gmail.com>
2026-04-20 15:10:59 +0200

    Create SetupPanel.cs

---
+++ SetupPanel.cs
@@ -0,0 +1,16 @@
+﻿using CMS2026UITKFramework;
+
+internal class SetupPanel
+{
+    private const string PANEL_ID = "NGH_Setup";
+    private UIPanel _panel;
+    private bool _isVisible;
+
+    public bool IsVisible => _isVisible;
+    public void Open() { _isVisible = true; _panel?.SetVisible(true); }
+    public void Close() { _isVisible = false; _panel?.SetVisible(false); }
+    public void Toggle() { if (_isVisible) Close(); else Open(); }
+
+    public void Build() { /* tworzy panel, SetVisible(false) na końcu */ }
+    public void Refresh() { /* wypełnia etykiety aktualnym stanem */ }
+}
